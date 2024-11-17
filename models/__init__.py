8ec35447067d94d830a1a3cfaa5f38f37cb6ae4a
mimo-oo <fadipemichael6@gmail.com>
2024-11-16 20:40:49 -0800

    models

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-from models.engine.file_storage import fileStorage
-
-storage = FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,9 @@
 """
 Defines a base model class.
 """
-
+import models
 from uuid import uuid4
 from datetime import datetime
-from AirBnB_clone import models
 
 
 class BaseModel:
@@ -47,9 +46,7 @@ with the current datetime
         self.updated_at = datetime.now()
         models.storage.save()
 
-
     def to_dict(self):
-
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
